bec6eaaca1e6b4552fcf050eb322879328b5cbcd
marinalf <marinalf@msn.com>
2023-07-07 15:30:35 -0400

    provider update

--- 1 - hub-vnet-networking/data_sources.tf
+++ 1 - hub-vnet-networking/data_sources.tf
@@ -9,9 +9,7 @@ data "aci_cloud_account" "aci_cloud_account_infra" {
   vendor     = "azure"
 }
 
-/*
 data "aci_cloud_context_profile" "hub_vnet" {
-  tenant_dn  = data.aci_tenant.infra_tenant.id
-  name       = "ct_ctxprofile_japaneast" # Hub VNet Cloud Context Profile (overlay-1)
+  tenant_dn = data.aci_tenant.infra_tenant.id
+  name      = "ct_ctxprofile_japaneast" # Hub VNet Cloud Context Profile (overlay-1)
 }
-*/
--- 1 - hub-vnet-networking/main.tf
+++ 1 - hub-vnet-networking/main.tf
@@ -3,26 +3,21 @@
 
 resource "aci_vrf" "services_vrf" {
   tenant_dn = data.aci_tenant.infra_tenant.id # Secondary VRF to host new CIDRs within Hub VNet.
-  name = var.services_vrf
+  name      = var.services_vrf
 }
 
 /*
-
 ## Issue: https://github.com/CiscoDevNet/terraform-provider-aci/issues/1049
 
-# Disable VNet Peering (Hub Networking)
-
-Location in GUI: Application Management » Cloud Context Profiles » ct_ctxprofile_japaneast
-
-resource "aci_rest_managed" "disable_hub_networking" {
+resource "aci_rest_managed" "disable_enable_hub_networking" {
   dn       = "uni/tn-infra/infranetwork-default/intnetwork-default/provider-azure-region-japaneast/regiondetail"
   class_name = "cloudtemplateRegionDetail"
   content = {
-    "hubNetworkingEnabled" = "no"
+    "hubNetworkingEnabled" = "yes" # Disable/Enable VNet Peering (Hub Networking)
   }
 }
 
-## Issue: https://github.com/CiscoDevNet/terraform-provider-aci/issues/1050
+*/
 
 # Firewall CIDR/Subnets
 
@@ -33,10 +28,9 @@ resource "aci_cloud_cidr_pool" "fw_cidr" {
 }
 
 resource "aci_cloud_subnet" "fw_subnets" {
-  for_each           = var.fw_subnets
-  cloud_cidr_pool_dn = aci_cloud_cidr_pool.fw_cidr.id
-  name               = each.value.name
-  ip                 = each.value.ip
+  for_each                        = var.fw_subnets
+  cloud_cidr_pool_dn              = aci_cloud_cidr_pool.fw_cidr.id
+  name                            = each.value.name
+  ip                              = each.value.ip
+  relation_cloud_rs_subnet_to_ctx = aci_vrf.services_vrf.id
 }
-
-*/
